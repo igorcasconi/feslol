27faf38e6e6b7ed7d73a779f3bf494b8dc1a08b2
Igor Casconi <igor492@gmail.com>
2020-12-27 16:29:00 -0300

    feat(topbar): added structure and style topbar

---
+++ src/components/TopBar/style.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const TitleLogo = styled.h1`
+  color: #fff;
+  font-size: 36px;
+  font-weight: bold;
+`
---
+++ src/screens/Home/style.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const RowDiv = styled.div`
+  position: relative;
+  width: 100vw;
+  height: 100vh;
+  flex: 1;
+`
