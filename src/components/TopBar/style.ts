8872ea4041202b76baff041d33eebb3a5dc0e183
Igor Casconi <igor492@gmail.com>
2020-12-28 15:24:12 -0300

    feat(topbar): added input search

--- src/components/TopBar/style.ts
+++ src/components/TopBar/style.ts
@@ -1,3 +1,4 @@
+import { createStyles, fade, makeStyles, Theme } from '@material-ui/core'
 import styled from 'styled-components'
 
 export const TitleLogo = styled.h1`
@@ -11,3 +12,26 @@ export const TextMenu = styled.p`
   text-transform: capitalize;
   font-family: Poppins;
 `
+
+export const useStylesReddit = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      border: '1px solid #e2e2e1',
+      width: 125,
+      padding: 5,
+      overflow: 'hidden',
+      borderRadius: 8,
+      backgroundColor: '#fcfcfb',
+      transition: theme.transitions.create(['border-color', 'box-shadow']),
+      '&:hover': {
+        backgroundColor: '#fff'
+      },
+      '&$focused': {
+        backgroundColor: '#fff',
+        boxShadow: `${fade('#004E96', 0.25)} 0 0 0 2px`,
+        borderColor: '#004E96'
+      }
+    },
+    focused: {}
+  })
+)
