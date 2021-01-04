34a21d79739d4afde09c15c860568bbd329f70b7
Igor Casconi <igor492@gmail.com>
2021-01-03 23:35:39 -0300

    feat(breadcrumb): added component

--- src/screens/News/ListNews/ListNews.tsx
+++ src/screens/News/ListNews/ListNews.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { Column } from 'components'
 
-const ListNews: React.FC = () => <Column width='100%' height='100%'></Column>
+const ListNews: React.FC = () => (
+  <Column width='100%' height='100%' paddingX={['10px', '50px', '250px', '350px', '450px']} paddingY='25px'></Column>
+)
 
 export default ListNews
